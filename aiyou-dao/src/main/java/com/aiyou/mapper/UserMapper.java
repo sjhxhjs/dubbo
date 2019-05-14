41bc5af05079caea138313e1edc529d8b05beaaa
abird <763152431@qq.com>
2019-05-14 21:40:33 +0800

    增加用户注册服务通过dubbo暴露借口,用户注册消费者代码,调用dubbo服务

--- aiyou-dao/src/main/java/com/aiyou/mapper/UserMapper.java
+++ aiyou-dao/src/main/java/com/aiyou/mapper/UserMapper.java
@@ -4,7 +4,9 @@ import com.aiyou.entity.User;
 import com.aiyou.entity.UserExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository("userMapper")
 public interface UserMapper {
     int countByExample(UserExample example);
 
--- aiyou-pojo/src/main/java/com/aiyou/entity/User.java
+++ aiyou-pojo/src/main/java/com/aiyou/entity/User.java
@@ -1,8 +1,10 @@
 package com.aiyou.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1473219175896351363L;
     private Integer uid;
 
     private String username;
